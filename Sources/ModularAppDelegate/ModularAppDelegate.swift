47d42d518f72af3d94cb63098b277f70621722bf
Ryan Schumacher <steg132@gmail.com>
2019-07-07 21:27:49 -0400

    Some Initial Changes

--- Sources/ModularAppDelegate/ModularAppDelegate.swift
+++ Sources/ModularAppDelegate/ModularAppDelegate.swift
@@ -1,3 +1,16 @@
-struct ModularAppDelegate {
-    var text = "Hello, World!"
+
+#if canImport(UIKit)
+import UIKit
+
+open class ModularAppDelegate: UIResponder, UIApplicationDelegate {
+
+    let modules: [UIApplicationDelegate] = []
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
 }
+
+#endif
