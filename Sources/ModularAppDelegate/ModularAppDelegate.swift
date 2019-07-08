94b3a1b2a5ea170e7e30de55960cb868f54191d9
Ryan Schumacher <steg132@gmail.com>
2019-07-07 21:37:43 -0400

    Making method open

--- Sources/ModularAppDelegate/ModularAppDelegate.swift
+++ Sources/ModularAppDelegate/ModularAppDelegate.swift
@@ -6,7 +6,7 @@ open class ModularAppDelegate: UIResponder, UIApplicationDelegate {
 
     var modules: [UIApplicationDelegate] = []
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    open func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
