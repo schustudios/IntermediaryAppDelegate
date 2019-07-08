12986cebedd7aca6e8502e9ed2ac8daa897439fd
Ryan Schumacher <steg132@gmail.com>
2019-07-07 22:25:12 -0400

    Making Modules read only

--- Sources/ModularAppDelegate/ModularAppDelegate.swift
+++ Sources/ModularAppDelegate/ModularAppDelegate.swift
@@ -4,7 +4,7 @@ import UIKit
 
 open class ModularAppDelegate: UIResponder, UIApplicationDelegate {
 
-    open var modules: [UIApplicationDelegate] = []
+    open var modules: [UIApplicationDelegate] { return [] }
 
     open func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Default Return
