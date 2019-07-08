7e32d8ee9620e5c57a08d35d57beed074dce2338
Ryan Schumacher <steg132@gmail.com>
2019-07-07 21:28:16 -0400

    changing let to var

--- Sources/ModularAppDelegate/ModularAppDelegate.swift
+++ Sources/ModularAppDelegate/ModularAppDelegate.swift
@@ -4,7 +4,7 @@ import UIKit
 
 open class ModularAppDelegate: UIResponder, UIApplicationDelegate {
 
-    let modules: [UIApplicationDelegate] = []
+    var modules: [UIApplicationDelegate] = []
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
